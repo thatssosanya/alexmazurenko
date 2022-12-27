aac580cb0eefe3172a9dda584a71d5125ec2a132
stonekite <feelsknight@gmail.com>
2022-12-27 03:20:41 +0300

    fixed fontawesome fouc

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/styles.css'
 
 // import fontawesome icons
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faGithub, faLinkedin, faWhatsapp, faTelegram
@@ -10,12 +11,13 @@ import {
   faAngleUp, faAngleDown, faAngleLeft, faAngleRight, faHouseUser, faTasks,
   faFistRaised, faUser, faAt, faEnvelope 
 } from '@fortawesome/free-solid-svg-icons'
-
 library.add(
   faGithub, faLinkedin, faWhatsapp, faTelegram, faAngleUp, faAngleDown,
   faAngleLeft, faAngleRight, faHouseUser, faTasks, faFistRaised, faUser, faAt,
   faEnvelope
 )
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
 
 // suppress warnings concerning useLayoutEffect during static generation
 if (typeof window === "undefined") React.useLayoutEffect = () => {}
